2b11c4d8d35c545df7f30fa4eea6e2b90744135b
Luis140494 <49540508+Luis140494@users.noreply.github.com>
2024-11-19 01:24:56 -0300

    Aperfeiçoando o código

--- app/api/v1/endpoints/datafonte.py
+++ app/api/v1/endpoints/datafonte.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-from sqlalchemy import column
+from sqlalchemy.exc import SQLAlchemyError
 
 from schemas.data_fonte_schema import DataFonteSchema
 from core.deps import get_session_Fonte
@@ -16,7 +16,7 @@ router = APIRouter()
 
 @router.get('/', response_model=List[DataFonteSchema])
 async def get_data(start: datetime, end: datetime, 
-                   variables: Optional[List[str]] = Query([]), #ambient_temperature,wind_speed,power
+                   variables: Optional[str] = None, #ambient_temperature,wind_speed,power
                    db: AsyncSession = Depends(get_session_Fonte)):
 
         async with db as session:
@@ -24,22 +24,47 @@ async def get_data(start: datetime, end: datetime,
                 # Para cada variável solicitada, verifica se existe no objeto Data
                 # Retorna os dados filtrados, onde cada variável é um campo
                 if variables:
-                    colunas_desejadas = variables[0].split(',')
-                    colunas_para_selecionar = [
-                                    getattr(DataFonteModel, coluna) 
-                                    for coluna in colunas_desejadas 
-                                    if hasattr(DataFonteModel, coluna)]
-        
-                    query = select(DataFonteModel).where(DataFonteModel.timestamp.between(start, end)).with_only_columns(*colunas_para_selecionar)
-                else:
-                    query = select(DataFonteModel).where(DataFonteModel.timestamp.between(start, end))  
+                    allowed_variables = set(variables.split(","))
+                    print(f"Variáveis recebidas: {allowed_variables}")  # Para debug
 
-                result = await session.execute(query)
-                datas: List[DataFonteModel] = result.scalars().unique().all()
+                    columns = set(DataFonteModel.__table__.columns.keys())
+                    print(f"Colunas válidas no banco: {columns}")  # Para debug
+
+                    invalid_columns = allowed_variables - columns
+                    if invalid_columns:
+                        raise HTTPException(status_code=400, detail=f"Colunas inválidas: {', '.join(invalid_columns)}")
+
+                    # Adicionar filtros de variáveis na consulta
+                    selected_columns = []
+                    selected_columns.append(DataFonteModel.id)
+                    selected_columns.append(DataFonteModel.timestamp)
+                    if 'wind_speed' in allowed_variables:
+                        selected_columns.append(DataFonteModel.wind_speed)
+                    if 'power' in allowed_variables:
+                        selected_columns.append(DataFonteModel.power)
+                    if 'ambient_temperature' in allowed_variables:
+                        selected_columns.append(DataFonteModel.ambient_temperature)
+
+                    # Usar *args para passar as colunas selecionadas como argumentos separados
+                    query = select(DataFonteModel).with_only_columns(*selected_columns).where(DataFonteModel.timestamp.between(start, end))
+                    #query = query.with_only_columns([col for col in DataFonteModel.__table__.columns if col.name != 'power'])
+                    print(query)
+                    result = await session.execute(query)
+                    datas: List[DataFonteModel] = result.fetchall()
+                    print(datas)
+                else:
+                    query = select(DataFonteModel).where(DataFonteModel.timestamp.between(start, end))
+                    result = await session.execute(query)
+                    datas: List[DataFonteModel] = result.scalars().unique().all()
+
                 if datas: 
                     return datas    
                 else:
-                    raise HTTPException(detail='Valores não encontradas.', status_code=status.HTTP_404_NOT_FOUND)     
+                    raise HTTPException(detail='Valores não encontradas.', status_code=status.HTTP_404_NOT_FOUND)
+            except SQLAlchemyError as e:
+                # Captura erros do SQLAlchemy e retorna uma mensagem genérica
+                raise HTTPException(status_code=500, detail="Erro ao consultar o banco de dados. Detalhes: " + str(e))     
             except Exception as e:
                 print(f"Erro ao buscar os dados: {e}")
+            
             
--- app/models/data_fonte_model.py
+++ app/models/data_fonte_model.py
@@ -10,3 +10,6 @@ class DataFonteModel(settings.DBBaseModel):
     power = Column(Float, nullable=False, default=0) 
     ambient_temperature = Column(Float, nullable=False, default=0) 
 
+    def __repr__(self):
+        return f"<DataFonteModel(id={self.id}, timestamp={self.timestamp}, wind_speed={self.wind_speed}, power={self.power}, ambient_temperature={self.ambient_temperature})>"
+
